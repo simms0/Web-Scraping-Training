560e21994a93ebf2de25bc32d3595b0cc0d28c5e
simon sokyrko <simonsokyrko@gmail.com>
2021-04-07 10:41:29 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	Moved _Scrape-Tutorial/README.md to ./
    	new file:   _app/helpers.js
    	new file:   _app/scotch.js
    	Added dependcy packages:  Express Cheerio lodash axios
    	new file:   package-lock.json
    	new file:   package.json
    	renamed:    _Scrape-Tutorial/server.js -> server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+/_ server.js _/
+//Creates and configures the HTTP server for the Scotch.io scraper
+
+// Require dependencies
+const logger = require('morgan');
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Configure the app port
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+// Load middlewares
+app.use(logger('dev'));
+
+// Add the Scotch author profile route
+app.get('/scotch/:author', (req, res, next) => {
+  const author = req.params.author;
+  sendResponse(res)(fetchAuthorProfile(author));
+});
+
+// Start the server and listen on the preconfigured port
+app.listen(port, () => console.log(`App started on port ${port}.`));
